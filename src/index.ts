b1c086777b0eb96100e3245bb41e3c5f8cd68214
Maxter <chiguas5000@hotmail.com>
2022-04-02 15:12:26 -0600

    asdf

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 const prisma = new PrismaClient();
 
 app.get("/products", async (req, res) => {
@@ -18,6 +20,22 @@ app.get("/products", async (req, res) => {
     take: limit,
   });
   res.send(products);
+})
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        lastName: req.body.lastName,
+        name: req.body.name,
+        password: req.body.password
+      }
+    })
+    res.send(user);
+  } catch {
+    res.status(500).send({ error: true });
+  }
 })
 
 app.listen(3000, () => {
