e185002978edd39c88734ef52e3ea6aae99117e8
Maxter <chiguas5000@hotmail.com>
2022-04-08 23:05:32 -0500

    newEndpoints

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { idText } from "typescript";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const prisma = new PrismaClient();
+
+const prisma = new PrismaClient({log: ['query']});
 
 app.get("/products", async (req, res) => {
   const sortByViews = req.query.sortByViews;
@@ -22,6 +24,15 @@ app.get("/products", async (req, res) => {
   res.send(products);
 })
 
+app.get("/product/:productId", async (req, res) => {
+  const product = await prisma.product.findFirst({
+    where:{
+      id: Number(req.params.productId),
+    }
+  });
+  res.send(product);
+})
+
 app.post("/users", async (req, res) => {
   try {
     const user = await prisma.user.create({
@@ -33,6 +44,38 @@ app.post("/users", async (req, res) => {
       }
     })
     res.send(user);
+  } catch {
+    res.status(500).send({ error: true });
+  }
+})
+
+app.post("/login", async (req, res) => {
+  try{
+    const user = await prisma.user.findFirst({
+      where: {
+        email: req.body.email,
+        password: req.body.password,
+      }
+    })
+    console.log(user);
+    res.send(user);
+  }catch{
+    res.status(500).send({ error: true });
+  }
+})
+
+app.post("/product-view", async (req, res) => {
+  try {
+    await prisma.product.update({
+      where:{
+        id: Number(req.body.productId),
+      },
+      data: {
+        views: {increment: 1}
+      }
+    })
+  
+    res.send({ ok: true });
   } catch {
     res.status(500).send({ error: true });
   }
